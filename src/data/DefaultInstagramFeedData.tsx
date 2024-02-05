7c245445f3d94cab162d3ff2965ee2b41f6401fa
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-02-05 12:06:52 +0530

    insta feed

--- src/data/DefaultInstagramFeedData.tsx
+++ src/data/DefaultInstagramFeedData.tsx
@@ -1,43 +1,39 @@
 const defaultFeedDataArray = [
     {
-        src: "/_next/static/media/newM2.7a6a7ee4.jpg",
-        height: 860,
-        width: 1290,
-        blurDataURL: "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewM2.7a6a7ee4.jpg&w=8&q=70",
-        blurWidth: 8,
-        blurHeight: 5
+        "src": "/_next/static/media/newA4.aed1340f.jpg",
+        "height": 4000,
+        "width": 6000,
+        "blurDataURL": "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewA4.aed1340f.jpg&w=8&q=70",
+        "blurWidth": 8,
+        "blurHeight": 5
     },
     {
-
-        src: "/_next/static/media/kaju.31246ecf.png",
-        height: 620,
-        width: 620,
-        blurDataURL: "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkaju.31246ecf.png&w=8&q=70",
-        blurWidth: 8,
-        blurHeight: 8
-
-    },
-
-    {
-
-        src: "/_next/static/media/newFD1.522c1139.jpg",
-        height: 1125,
-        width: 1500,
-        blurDataURL: "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewFD1.522c1139.jpg&w=8&q=70",
-        blurWidth: 8,
-        blurHeight: 6
-
+        "src": "/_next/static/media/newA4.aed1340f.jpg",
+        "height": 4000,
+        "width": 6000,
+        "blurDataURL": "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewA4.aed1340f.jpg&w=8&q=70",
+        "blurWidth": 8,
+        "blurHeight": 5
     },
     {
-        src: "/_next/static/media/newA4.aed1340f.jpg",
-        height: 4000,
-        width: 6000,
-        blurDataURL: "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewA4.aed1340f.jpg&w=8&q=70",
-        blurWidth: 8,
-        blurHeight: 5
+        "src": "/_next/static/media/newA4.aed1340f.jpg",
+        "height": 4000,
+        "width": 6000,
+        "blurDataURL": "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewA4.aed1340f.jpg&w=8&q=70",
+        "blurWidth": 8,
+        "blurHeight": 5
     },
+    {
+        "src": "/_next/static/media/newA4.aed1340f.jpg",
+        "height": 4000,
+        "width": 6000,
+        "blurDataURL": "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FnewA4.aed1340f.jpg&w=8&q=70",
+        "blurWidth": 8,
+        "blurHeight": 5
+    }
 ];
 
 const hashtag = '#feedback'
+
 
 export { defaultFeedDataArray, hashtag };
