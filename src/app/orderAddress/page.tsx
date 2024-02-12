67b9309dfc01a9ffd6d3bdacf72f14e6a46bfecb
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-02-12 16:44:20 +0530

    adjust footer

--- src/app/orderAddress/page.tsx
+++ src/app/orderAddress/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+import CopyRight from '@/component/copyRight/CopyRight';
+import Footer from '@/component/footer/Footer';
+import Header from '@/component/headerSection/Header';
 import OrderAddress from '@/component/orderAddress/OrderAddress';
 import React from 'react';
 
@@ -7,7 +10,10 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
+            <Header />
             <OrderAddress />
+            <Footer />
+            <CopyRight />
         </div >
     );
 }
