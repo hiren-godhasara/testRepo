6c4aa77bc1e8ab1976fbcac297aee4750883512b
Hiren <hiren.godhasara@gmail.com>
2024-02-17 12:08:02 +0530

    homepage

--- src/component/homePageSection/HomePage.tsx
+++ src/component/homePageSection/HomePage.tsx
@@ -1,24 +1,32 @@
+import { Carousel } from 'antd';
 import styles from './HomePage.module.scss';
-import HomePageSlider from './HomePageSlider';
+import Image from 'next/image';
+import { homePage1, homePage2, homePage3, homePage4, homePage5, homePage6 } from '@/S3Images/S3Images';
+import i1 from '../../imageFolder/mdfh1.jpg'
+import i2 from '../../imageFolder/mdfh2.jpg'
+import i3 from '../../imageFolder/mdfh3.jpg'
+import i4 from '../../imageFolder/mdfh4.jpg'
+import i5 from '../../imageFolder/mdfh5.jpg'
+import i6 from '../../imageFolder/mdfh6.jpg'
 
-const whatsappLink = 'https://wa.me/+919157059719';
 const HomePage = () => {
 
     return (
         <div className={styles.home}>
-            <HomePageSlider />
-            <div className={styles.p}>
-                <p className={styles.p1}>Start Your Dry With Our </p>
-                <p className={styles.p2}> Fresh Dryfruits....</p>
+            <div className={styles.productCarousel}>
+                <Carousel slidesToShow={1} autoplay autoplaySpeed={4500} speed={2000}  >
+                    <Image src={i1} alt={`Image`} width={1423} height={740} className={styles.image} />
+                    <Image src={i2} alt={`Image`} width={1423} height={740} className={styles.image} />
+                    <Image src={i3} alt={`Image`} width={1423} height={740} className={styles.image} />
+                    <Image src={i4} alt={`Image`} width={1423} height={740} className={styles.image} />
+                    <Image src={i5} alt={`Image`} width={1423} height={740} className={styles.image} />
+                    <Image src={i6} alt={`Image`} width={1423} height={740} className={styles.image} />
+                </Carousel>
             </div>
 
-            <a href={whatsappLink}><button className={styles.rightSection}><span>Chat with us</span></button></a>
+            <button className={styles.rightSection}>Contact Us</button>
         </div>
     );
 };
 
 export default HomePage;
-
-
-
-
