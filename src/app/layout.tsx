917ec776a3259fd33a8d246c94f37853b14017c0
Hiren <78787734+hiren-godhasara@users.noreply.github.com>
2024-02-20 09:36:38 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import Script from "next/script";
+import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +20,9 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
+         <Head>
+          <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+        </Head>
         <body className={inter.className}>
           {children}
         </body>
