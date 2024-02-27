983b028c33a50c3940cb09d7d183807645ba4a0f
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-02-27 11:25:05 +0530

    outlet info

--- src/component/bestSellingSection/BestSellingProducts.tsx
+++ src/component/bestSellingSection/BestSellingProducts.tsx
@@ -5,7 +5,7 @@ const BestSelling = () => {
         <div className={styles.container}>
             <div className={styles.main}>
                 <p className={styles.head}>Our Best Selling Products</p>
-                <p className={styles.description}>Indulge in exquisite luxury with our best-selling dry fruit medley, a harmonious blend of premium nuts and dried fruits. Elevate your snacking experience with a delectable combination thats rich in flavor and packed with wholesome goodness.</p>
+                <p className={styles.description}>Enjoy the pinnacle of taste and nutrition with our top-selling blend of dried fruits and nuts. Each bite is a harmonious fusion of flavors, crafted to delight your senses. Savor the richness of our premium ingredients and elevate your snacking experience to new heights of satisfaction.</p>
             </div>
         </div>
     );
