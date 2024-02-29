488324d189f47bbeb3dda860b429913d77dc63dd
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-02-29 13:03:32 +0530

    bg color

--- src/app/privacyPolicy/page.tsx
+++ src/app/privacyPolicy/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Cart() {
     return (
-        <div>
+        <div style={{ backgroundColor: 'white' }}>
             <PrivacyPolicy />
         </div>
     );
