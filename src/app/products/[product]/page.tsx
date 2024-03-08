192678be471a7ecbe0d295072c71d1a0e5ca7dd7
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-03-08 14:10:54 +0530

    ordering dryfruits

--- src/app/products/[product]/page.tsx
+++ src/app/products/[product]/page.tsx
@@ -1,8 +1,7 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { usePathname } from 'next/navigation';
 import { DryFruitSliderForOrder } from '@/component/orderingDryFruits/OrderingDryFruits';
-import { useSearchParams, usePathname } from 'next/navigation';
-import { getToken } from '@/getLocalStroageToken';
 
 export interface Product {
     _id: any;
@@ -20,47 +19,49 @@ export interface Product {
 const Cart = () => {
 
 
-    const [productDetails, setProductDetails] = useState<Product | null>(null);
     const desiredPart = usePathname();
     const parts = desiredPart.split('/products/');
     const paramId = parts[1];
+    console.log(paramId);
 
-    const token = getToken()
+    // const [productDetails, setProductDetails] = useState<Product | null>(null);
 
-    useEffect(() => {
-        getProductDetails();
-    }, []);
+    // const token = getToken()
 
-    const getProductDetails = async () => {
-        const isOrderRedirecting = typeof window !== 'undefined' ? localStorage.getItem("isOrderRedirecting") : null;
-        if (isOrderRedirecting === "true") {
-            window.location.reload()
-            localStorage.removeItem("isOrderRedirecting")
-        }
-        if (!paramId) return;
-        try {
-            const response = await fetch(`${process.env.BASE_URL}/s/product/${paramId}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
+    // useEffect(() => {
+    //     getProductDetails();
+    // }, []);
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
+    // const getProductDetails = async () => {
+    //     const isOrderRedirecting = typeof window !== 'undefined' ? localStorage.getItem("isOrderRedirecting") : null;
+    //     if (isOrderRedirecting === "true") {
+    //         window.location.reload()
+    //         localStorage.removeItem("isOrderRedirecting")
+    //     }
+    //     if (!paramId) return;
+    //     try {
+    //         const response = await fetch(`${process.env.BASE_URL}/s/product/${paramId}`, {
+    //             method: 'GET',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //                 'Authorization': `Bearer ${token}`,
+    //             },
+    //         });
 
-            const data = await response.json();
-            setProductDetails(data.data[0]);
-        } catch (error) {
-            console.error('There was a problem fetching the data:', error);
-        }
-    };
+    //         if (!response.ok) {
+    //             throw new Error('Network response was not ok');
+    //         }
+
+    //         const data = await response.json();
+    //         setProductDetails(data.data[0]);
+    //     } catch (error) {
+    //         console.error('There was a problem fetching the data:', error);
+    //     }
+    // };
 
     return (
         <div style={{ backgroundColor: 'white' }}>
-            {productDetails && <DryFruitSliderForOrder data={productDetails} />}
+            {paramId && <DryFruitSliderForOrder data={paramId} />}
         </div>
     );
 };
