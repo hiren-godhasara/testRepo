f78347b304961e96894e1e68a920e071a971ef40
Hiren <hiren.godhasara@gmail.com>
2024-03-09 11:01:52 +0530

    instagram feeds

--- src/component/instagramFeed/HashtagInstagramFeed.tsx
+++ src/component/instagramFeed/HashtagInstagramFeed.tsx
@@ -1,5 +1,6 @@
+'use client'
 import React, { useEffect, useState } from 'react';
-import styles from './HashtagInstagramFeed.module.scss';
+import styles from './InstagramFeed.module.scss';
 import axios from 'axios';
 import { Carousel } from 'antd';
 import Image from 'next/image';
@@ -7,10 +8,10 @@ import companyLogo from '../../imageFolder/mdfLogo.png'
 import like from '../../imageFolder/heart.png'
 import comment from '../../imageFolder/comment.png'
 import share from '../../imageFolder/share.png'
-import save from '../../imageFolder/saveIcon.png'
 import dots from '../../imageFolder/dots.png'
-import { defaultFeedDataArray, hashtag } from '../../data/DefaultInstagramFeedData'
+import save from '../../imageFolder/saveIcon.png'
 import Link from 'next/link';
+
 
 interface InstagramFeed {
     id: string;
@@ -35,46 +36,77 @@ const fetchInstagramFeeds = async (accessToken: string, count: Number) => {
 
 const HashtagInstagramFeeds: React.FC = () => {
     const [feeds, setFeeds] = useState<InstagramFeed[]>([]);
-    const accessToken = 'IGQWRNYV8taVlacmptekFYdlJGUGtTbWdIVW1FRWNNY2dNR2JwYzVVdFA5TnZAUM1VDUkdfR0pwRGgxZAF9RN09JNjhoeUtMZAl8talNjZAElOelZAuVERpc3ZAXZAWlSNmJ1cWJoeUtnM2lteWZA0Y08xXzhlMWpaX3VfajAZD';
+    const [slidesToShow, setSlidesToShow] = useState<number>(3);
+
+    const accessToken = 'IGQWRNS2RYcW9yUFhXNEg5b19RLWo5aHZA1OWRfMUkzQXFONnZALT0VMcnY5X1NOUkRvemlTb1VVTDhZAaC12X3pCZAHZASbUpNZAFQ4Q3psSl9FNkRjQWRqOUdkYzFIa3FBa0VFbFdyNnFIV0ZAzdzk4bkw2cEFDOUVTemMZD';
+
+
+    const handleResize = () => {
+        const windowWidth = window.innerWidth;
+        if (windowWidth < 765) {
+            setSlidesToShow(1);
+        } else if (windowWidth < 990) {
+            setSlidesToShow(2);
+        } else if (windowWidth < 1280) {
+            setSlidesToShow(3);
+        } else if (windowWidth < 1320) {
+            setSlidesToShow(3)
+        } else {
+            setSlidesToShow(4);
+        }
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     useEffect(() => {
         fetchInstagramFeeds(accessToken, 10)
             .then((data) => setFeeds(data))
             .catch((error) => console.error('Error setting Instagram feeds:', error));
     }, []);
+    console.log(feeds);
 
-    const filteredArray = feeds.filter(item => item.caption && item.caption.includes(hashtag));
-    const defaultCount = Math.max(0, 4 - filteredArray.length);
-    const defaultImages = Array.from({ length: defaultCount }, (_, index) => {
-        const defaultFeedData = defaultFeedDataArray[index % defaultFeedDataArray.length];
-        return {
-            id: `default-${index + 1}`,
-            media_url: defaultFeedData.src,
-            permalink: 'https://www.instagram.com/mydryfruit_com/',
-            caption: ''
-        };
-    });
-    const finalArray = [...filteredArray, ...defaultImages];
+    const filteredArray = feeds.filter(obj => obj.caption && obj.caption.includes("#feedback"));
+    const ary = [...filteredArray]
+    console.log(filteredArray);
+
+
+
 
     return (
-        <div className={styles.carouselWrapper}>
-            <h1 className={styles.h1}>Instagram Customers Feeds</h1>
-            <Carousel slidesToShow={4} autoplay autoplaySpeed={3500} speed={1500} dots={false} className={styles.card} >
-                {finalArray.map((feed) => (
-                    <div key={feed.id}>
-                        <Link href={feed.permalink} target="_blank" rel="noopener noreferrer">
-                            <div className={styles.separateCard}>
-                                <div className={styles.mainFlex}>
-                                    <div className={styles.flex}>
-                                        <Image src={companyLogo} alt={` Image`} width={100} height={100} className={styles.companyLogo} />
-                                        <p className={styles.userName}>mydryfruit_com</p>
+        <div className={styles.instagramFeedArea}>
+            <h1 className={styles.h2}>Instagram Customer Reviews</h1>
+            <Carousel
+                slidesToShow={slidesToShow}
+                autoplay
+                draggable
+                autoplaySpeed={3500}
+                speed={1500}
+                dots={false}
+                className={styles.card}
+            >
+                {ary.map((feed) => (
+                    <div key={feed.id} >
+                        <div key={feed.id} className={styles.feedBackContainer}>
+                            <Link href={feed.permalink} target="_blank" rel="noopener noreferrer">
+                                <div className={styles.feedBackWrapper}>
+                                    <div className={styles.feedBackHeader}>
+                                        <div style={{ marginLeft: '0.2rem' }}>
+                                            <Image src={companyLogo} alt={` Image`} width={50} height={50} className={styles.companyLogo} />
+                                        </div>
+                                        <p className={styles.feedBackHeading}>mydryfruit_com</p>
                                     </div>
-                                    <div className={styles.dots}>
-                                        <Image src={dots} alt={` Image`} width={25} height={25} className={styles.dots} />
+                                    <div>
+                                        <Image src={dots} alt={` Image`} width={25} height={25} style={{ marginRight: '0' }} />
                                     </div>
                                 </div>
-                                <div className={styles.imageFlex}>
-                                    <Image src={feed.media_url} width={205} height={205} alt={feed.caption} className={styles.image} />
+                                <div className={styles.feedBackImageArea}>
+                                    <Image src={feed.media_url} width={220} height={205} alt={feed.caption} className={styles.feedImg} />
                                 </div>
                                 <div className={styles.mainIcon}>
                                     <div className={styles.icons}>
@@ -86,18 +118,14 @@ const HashtagInstagramFeeds: React.FC = () => {
                                         <Image src={save} alt={` Image`} width={28} height={28} className={styles.save} />
                                     </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
                     </div>
-                ))}
+                ))
+                }
             </Carousel >
-        </div >
+        </div>
     );
 };
 
 export default HashtagInstagramFeeds;
-
-
-
-
-
