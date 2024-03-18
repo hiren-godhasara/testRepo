cea6a8cd4830fca5c618e3c67f9156460a394225
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-03-18 18:50:46 +0530

    metadata bug fixing

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -1,14 +1,15 @@
 import ContactUs from '@/component/GeneralPages/ContactUs';
 import React from 'react';
+const information = require('../../data/details.json')
 
 export const metadata = {
-    title: "My Dry Fruit-Contact Us",
-    description: "Reach out and connect with MyDryFruit to have your queries, feedback, or inquiries addressed promptly! Whether you're seeking information about our products, exploring partnership opportunities, or eager to share your experience, we're committed to providing attentive assistance. Don't hesitate to get in touch today and initiate a fruitful conversation with us!",
+    title: information?.contactUs?.title,
+    description: information?.contactUs?.description,
 };
+
 export default function Cart() {
     return (
         <div>
-
             <ContactUs />
         </div>
     );
