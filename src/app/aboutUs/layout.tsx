f71bb00b50481925f887e6b165f70cdcf11fc76e
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-03-20 18:05:17 +0530

    metadata

--- src/app/aboutUs/layout.tsx
+++ src/app/aboutUs/layout.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import Head from 'next/head';
 const information = require('../../data/details.json');
 
 export const metadata = {
     title: information?.aboutUs?.title,
     description: information?.aboutUs?.description,
-    image: information?.aboutUs?.image,
     openGraph: {
         title: information?.aboutUs?.ogTitle,
         description: information?.aboutUs?.ogDescription,
-        image: information?.aboutUs?.ogImage,
+        images: information?.aboutUs?.ogImage,
     },
 };
 
