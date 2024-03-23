6404b6a975b4721a2877a9d295df1584f92167b3
Hiren <hiren.godhasara@gmail.com>
2024-03-23 11:20:35 +0530

    tagline issue

--- src/component/tagLine/TagLine.tsx
+++ src/component/tagLine/TagLine.tsx
@@ -1,58 +1,76 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import styles from './TagLine.module.scss';
+
+
+
+// const TagLine = () => {
+//     const [tagLineList, setTagLineList] = useState<any>([]);
+//     const [currentTagLineIndex, setCurrentTagLineIndex] = useState(0);
+
+//     const fetchTagLineData = () => {
+//         fetch(`${process.env.BASE_URL}/s/tagLine`, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+
+//             },
+//         })
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 // console.log(data.data);
+//                 setTagLineList(data?.data);
+//             })
+//             .catch(error => {
+//                 console.error('There was a problem fetching the data:', error);
+//             });
+//     };
+
+//     useEffect(() => {
+//         fetchTagLineData();
+//     }, []);
+
+//     useEffect(() => {
+//         const intervalId = setInterval(() => {
+//             setCurrentTagLineIndex(prevIndex => (prevIndex + 1) % tagLineList?.length);
+//         }, 3000);
+
+//         return () => clearInterval(intervalId);
+//     }, [tagLineList]);
+
+//     // console.log(tagLineList, 'tagLineList');
+
+
+//     return (
+//         <>
+//             <div className={styles.container}>
+//                 {tagLineList?.length > 0 && (
+//                     <p className={styles.p}>
+//                         {(tagLineList[currentTagLineIndex].tagLine)}
+//                     </p>
+//                 )}
+//             </div>
+//         </>
+//     );
+// };
+
+// export default TagLine;
+
+
+
 import styles from './TagLine.module.scss';
-
-
-
 const TagLine = () => {
-    const [tagLineList, setTagLineList] = useState<any>([]);
-    const [currentTagLineIndex, setCurrentTagLineIndex] = useState(0);
-
-    const fetchTagLineData = () => {
-        fetch(`${process.env.BASE_URL}/s/tagLine`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                // console.log(data.data);
-                setTagLineList(data?.data);
-            })
-            .catch(error => {
-                console.error('There was a problem fetching the data:', error);
-            });
-    };
-
-    useEffect(() => {
-        fetchTagLineData();
-    }, []);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentTagLineIndex(prevIndex => (prevIndex + 1) % tagLineList?.length);
-        }, 3000);
-
-        return () => clearInterval(intervalId);
-    }, [tagLineList]);
-
-    // console.log(tagLineList, 'tagLineList');
-
 
     return (
         <>
             <div className={styles.container}>
-                {tagLineList?.length > 0 && (
-                    <p className={styles.p}>
-                        {(tagLineList[currentTagLineIndex].tagLine)}
-                    </p>
-                )}
+                <p className={styles.p}>
+                    Get 25% off in every Almond products.
+                </p>
             </div>
         </>
     );
