370c050737f3d57fdd24472b65b59a089116500f
anand-rize <78787734+hiren-godhasara@users.noreply.github.com>
2024-03-26 18:25:13 +0530

    width and height

--- src/app/(register-login-user)/login/page.tsx
+++ src/app/(register-login-user)/login/page.tsx
@@ -10,8 +10,8 @@ export const metadata = {
         description: information?.login?.ogDescription,
         images: {
             url: information?.login?.ogImage,
-            height: "200px",
-            width: "200px",
+            height: 200,
+            width: 200,
         },
     },
 };
